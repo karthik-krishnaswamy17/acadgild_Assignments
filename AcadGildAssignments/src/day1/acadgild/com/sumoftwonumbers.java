12d3f42e2fe156b00d844a43c9a80096c0e36f75
Karthik <karthik.krishnaswamy17@gmail.com>
2016-08-27 19:41:12 +0530

    Day 1 Assignment - sum of two numbers

---
+++ AcadGildAssignments/src/day1/acadgild/com/sumoftwonumbers.java
@@ -0,0 +1,25 @@
+package day1.acadgild.com;
+
+import java.util.Scanner;
+
+public class sumoftwonumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		System.out.println("Enter number one___");
+		int a = reader.nextInt();
+		System.out.println("Enter number two___");
+		int b = reader.nextInt();
+		System.out.println("Sum:" + sum(a, b));
+		reader.close();
+
+	}
+
+	private static int sum(int a, int b) {
+		// TODO Auto-generated method stub
+		return (a - (-b));
+	}
+
+}
